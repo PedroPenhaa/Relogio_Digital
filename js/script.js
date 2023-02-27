adb1c4dd662eb3ef733cf44fad976e615f79dad1
PedroPenha <pedro.eng98@gmail.com>
2023-02-27 18:46:43 -0300

    Init-Js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let relogio = document.querySelector('#relogio')
+
+let h = document.querySelector('#h')
+let m = document.querySelector('#m')
+let s = document.querySelector('#s')
+
+let hSmart = document.querySelector('#hSmart')
+let mSmart = document.querySelector('#mSmart')
+let sSmart = document.querySelector('#sSmart')
+
+let data = document.querySelector('#data')
+let semana = document.querySelector('#semana')
+
+let dataHora = new Date()
